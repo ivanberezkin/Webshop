b5ecd9b4c446fa3850e82012638adf72da7e038b
ivbe0111 <berezkinivan94@gmail.com>
2026-03-14 11:46:15 +0100

    added a navbarCreate.js instead of copy-pasting code in every html file

---
+++ navbarCreater.js
@@ -0,0 +1,37 @@
+const navbarCreaterHTML = `  <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+      <div class="container">
+        <a href="#" class="navbar-brand">Webshop</a>
+        <button
+          class="navbar-toggler"
+          data-bs-toggle="collapse"
+          data-bs-target="#nav"
+          aria-controls="nav"
+          aria-label="Expand Navigation"
+        >
+          <div class="navbar-toggler-icon"></div>
+        </button>
+        <div class="collapse navbar-collapse" id="nav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a href="#" class="nav-link active">Home</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Products</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Contact</a>
+            </li>
+          </ul>
+
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a href="#" class="nav-link">
+                <i class="bi bi-cart3"></i>
+                <span id="cart-count" class="badge bg-secondary">0</span>
+              </a>
+            </li>
+        </div>
+      </div>
+    </nav>`;
+
+document.querySelector("#navbar").innerHTML = navbarCreaterHTML;
