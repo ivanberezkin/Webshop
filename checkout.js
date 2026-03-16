acd3297a5620c52d911cf47fdea03770c68b089a
ivbe0111 <berezkinivan94@gmail.com>
2026-03-16 15:43:07 +0100

    added checkValidity for inputs in form

---
+++ checkout.js
@@ -0,0 +1,16 @@
+const form = document.querySelector("#checkout-form");
+const userAddedProducts = JSON.parse(localStorage.getItem("cart")) || [];
+const inputs = form.querySelectorAll("input");
+
+inputs.forEach((input) => {
+  input.addEventListener("input", () => {
+    if (input.checkValidity()) {
+      input.classList.remove("is-invalid");
+      input.classList.add("is-valid");
+    } else {
+      input.classList.remove("is-valid");
+      input.classList.add("is-invalid");
+    }
+  });
+});
+
