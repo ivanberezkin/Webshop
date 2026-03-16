d5e00801690801704b4ca85b445009e36d0cbb58
ivbe0111 <berezkinivan94@gmail.com>
2026-03-16 22:11:18 +0100

    changed to relative paths for import

--- Javascript/fetchProducts.js
+++ Javascript/fetchProducts.js
@@ -1,4 +1,4 @@
-import { updateCartIcon } from "/Javascript/utils.js";
+import { updateCartIcon } from "./utils.js";
 
 const container = document.querySelector("#product-container");
 let allProducts = [];
